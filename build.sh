d01873769e1e27398c89633c2707ab4697a001d3
luc <luc.leray@gmail.com>
2023-11-15 11:07:41 +0100

    set things up

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 #! /bin/sh
 
+# first, generate 100 random pages
+
 random_page_template_path=app/random-page-template/page.tsx
 random_pages_dir=app/random-pages
 
 rm -rf $random_pages_dir
 
-# generate 100 random pages
 for i in {1..100}
 do
   random_page_dir="$random_pages_dir/$i"
@@ -13,3 +14,25 @@ do
   mkdir -p $random_page_dir
   sed -e "s/random_page_i/$i/" -e "s/random_value/$RANDOM/" $random_page_template_path > $random_page_path
 done
+
+echo "Generated 100 random pages"
+
+# ====================================
+
+# then, build the project
+
+. ./build
+
+bench=public/bench.txt
+
+echo "starting build $build_id"
+echo "build_id=$build_id" > $bench
+echo "push_ts=$push_ts" >> $bench
+
+echo "start_ts=$(date +%s)" >> $bench
+
+npm run build-only
+
+echo "end_ts=$(date +%s)" >> $bench
+
+cat $bench
